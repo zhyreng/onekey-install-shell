84a26c13d2cdd0023c14f1a5462f7cf6d251fc5f
zhyreng <zhyreng@163.com>
2020-06-13 00:21:34 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FRPS_VER=0.33.0
-export FRPS_INIT="https://raw.githubusercontent.com/zhyr/onekey-install-shell/master/frps/frps.init"
-export aliyun_download_url="https://code.aliyun.com/zhyr/frp/raw/master"
+export FRPS_INIT="https://raw.githubusercontent.com/zhyreng/onekey-install-shell/master/frps/frps.init"
+export aliyun_download_url="https://code.aliyun.com/zhyreng/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
