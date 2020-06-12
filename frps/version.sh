0bb24f412bb34ab9003e214313852dca86ff5f1f
zhyreng <zhyreng@163.com>
2020-06-13 00:18:03 +0800

    初始化
    初始化frps一键更新

---
+++ frps/version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FRPS_VER=0.33.0
+export FRPS_INIT="https://raw.githubusercontent.com/zhyr/onekey-install-shell/master/frps/frps.init"
+export aliyun_download_url="https://code.aliyun.com/zhyr/frp/raw/master"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
